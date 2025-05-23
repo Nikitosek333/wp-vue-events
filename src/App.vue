0f4d51d2dc49031fe23060756fdbedd67baadbee
Nikitosek333 <Mlad8327uinad@gmail.com>
2025-05-23 16:41:17 +0300

    Initial commit: Vue frontend for WP events

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <EventList />
+  </div>
+</template>
+
+<script>
+import EventList from './components/EventList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EventList
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>События из WordPress</h1>
+    <ul v-if="events.length">
+      <li v-for="event in events" :key="event.id">
+        <h2>{{ event.title }}</h2>
+        <div v-html="event.content"></div>
+      </li>
+    </ul>
+    <p v-else>Загрузка событий...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      events: []
+    };
+  },
+  mounted() {
+    fetch('http://memosprite.local/wp-json/custom/v1/events')
+      .then(res => res.json())
+      .then(data => {
+        this.events = data;
+      })
+      .catch(err => console.error('Ошибка загрузки событий:', err));
+  }
+};
+</script>
+
